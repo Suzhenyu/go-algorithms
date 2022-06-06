692e71437e9c5331563c03d64782ea0726062a37
Suzhenyu <983424749@qq.com>
2022-06-06 16:19:51 +0800

    实现了『插入排序』

---
+++ sort/insertionsort.go
@@ -0,0 +1,15 @@
+package sort
+
+func InsertionSort(array []int) {
+	for i := 1; i < len(array); i++ {
+
+		current := array[i]
+
+		j := i - 1
+		for j >= 0 && array[j] > current {
+			array[j+1] = array[j]
+			j--
+		}
+		array[j+1] = current
+	}
+}
